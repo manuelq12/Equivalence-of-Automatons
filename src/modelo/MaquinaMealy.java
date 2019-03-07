c9139f9a05c5496f9eacfd7db6b966916c37acfb
manuelq12 <36927617+manuelq12@users.noreply.github.com>
2019-03-06 20:59:25 -0500

    Atributos Adicionales
    Atributos adicionales a la Clase Mealy

--- src/modelo/MaquinaMealy.java
+++ src/modelo/MaquinaMealy.java
@@ -4,11 +4,13 @@ public class MaquinaMealy {
 
 	private String[][] incidenceMatrix;
 	private String[] states;
+	private String[] outputs;
 	
 	public MaquinaMealy(int nStates, int nInputs) {
 		
 		incidenceMatrix =  new String[nStates][nInputs];
 		states = new String[nStates];
+		outputs = new String [nStates];
 	}
 
 	public String[][] getIncidenceMatrix() {
@@ -18,6 +20,23 @@ public class MaquinaMealy {
 	public void setIncidenceMatrix(String[][] incidenceMatrix) {
 		this.incidenceMatrix = incidenceMatrix;
 	}
+
+	public String[] getStates() {
+		return states;
+	}
+
+	public void setStates(String[] states) {
+		this.states = states;
+	}
+
+	public String[] getOutputs() {
+		return outputs;
+	}
+
+	public void setOutputs(String[] outputs) {
+		this.outputs = outputs;
+	}
+	
 	
 	
 
