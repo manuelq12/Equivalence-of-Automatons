13d407b869f01bffaf9c856a9c5f0cb10a523de1
manuelq12 <36927617+manuelq12@users.noreply.github.com>
2019-03-06 19:31:52 -0500

    Primeras adiciones Moore y Mealy
    Se adicionaron los atributos de matriz de incidencia, el constructor de cada clase, y se establecieron los GET and SET de los atributos correspondientes

--- src/modelo/MaquinaMealy.java
+++ src/modelo/MaquinaMealy.java
@@ -2,4 +2,21 @@ package modelo;
 
 public class MaquinaMealy {
 
+	private String[][] incidenceMatrix;
+	
+	public MaquinaMealy(int nEstados, int nEntradas) {
+		
+		incidenceMatrix =  new String[nEstados][nEntradas];
+	}
+
+	public String[][] getIncidenceMatrix() {
+		return incidenceMatrix;
+	}
+
+	public void setIncidenceMatrix(String[][] incidenceMatrix) {
+		this.incidenceMatrix = incidenceMatrix;
+	}
+	
+	
+
 }
--- src/modelo/MaquinaMoore.java
+++ src/modelo/MaquinaMoore.java
@@ -2,4 +2,29 @@ package modelo;
 
 public class MaquinaMoore {
 
+	private String[][] incidenceMatrix;
+	private String[] outputs;
+	
+	public MaquinaMoore(int nEstados, int nEntradas) {
+		incidenceMatrix = new String[nEstados][nEntradas];
+		outputs = new String [nEstados];
+	}
+
+	public String[][] getIncidenceMatrix() {
+		return incidenceMatrix;
+	}
+
+	public void setIncidenceMatrix(String[][] incidenceMatrix) {
+		this.incidenceMatrix = incidenceMatrix;
+	}
+
+	public String[] getOutputs() {
+		return outputs;
+	}
+
+	public void setOutputs(String[] outputs) {
+		this.outputs = outputs;
+	}
+	
+	
 }
